8cb4dec5830117f9af5c162db6f2acb58857d737
piyyu <piyushkr446@gmail.com>
2026-02-23 14:40:54 +0530

    restart project

--- packages/ui/src/index.ts
+++
@@ -1,6 +0,0 @@
-// Re-export shared UI utilities and components
-
-export { cn } from './utils'
-export * from './components/badge'
-export * from './components/button'
-export * from './components/card'
--- packages/vc-core/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './types'
-export * from './issuer'
-export * from './verifier'
